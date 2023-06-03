9f5984928b4599d1717e6b2949be12a35c6e5c63
hhj <haohuaijin@gmail.com>
2023-06-03 23:18:59 +0800

    139

---
+++ hot100/139.WordBreak.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        auto set = unordered_set<string> ();
+        for (auto word: wordDict) {
+            set.insert(word);
+        }
+
+        auto dp = vector<bool>(s.size() + 1);
+        dp[0] = true;
+        for (int i = 1; i <= s.size(); ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (dp[j] && set.count(s.substr(j, i - j))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.size()];
+    }
+};
