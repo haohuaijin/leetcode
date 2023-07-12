42a8ea2885e617d485b827c3d1f73f571dd75267
hhj <haohuaijin@gmail.com>
2023-07-12 15:24:16 +0800

    448

---
+++ hot100/448.FindAllNumbersDisappearedInAnArray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> res;
+        for(int i = 0; i < nums.size(); i++){
+            int num = abs(nums[i]);
+            if(nums[num - 1] > 0){
+                nums[num - 1] *= -1;
+            }
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] > 0){
+                res.push_back(i + 1);
+            }
+        }
+        return res;
+    }
+};
